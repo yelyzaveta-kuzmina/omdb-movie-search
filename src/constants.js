ead2cdd03b77eceaa103f01a45396096c65c7a9c
Lisa <elizaveta.kzm@gmail.com>
2021-12-09 10:54:48 +0100

    House of Gucci - imdbID

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 export const randomlySuggestedMovie = {
   Title: "House of Gucci",
   Year: 2021,
+  imdbID: "tt11214590",
   Poster:
     "https://m.media-amazon.com/images/M/MV5BZThjMTA5YjgtZmViZi00YjY0LTk5MzQtMjUwMGEzZGVlYzFjXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg",
 };
