0e999c6b16751296b0ec6f2d8b1e7945aabab605
Lisa <elizaveta.kzm@gmail.com>
2021-12-09 04:05:25 +0100

    MovieDetails with styles

---
+++ src/components/movieDetails/movieDetails.jsx
@@ -0,0 +1,25 @@
+import styles from "./styles.module.scss";
+
+const Block = ({ label, value }) => {
+  return (
+    <p>
+      <b>{label}: </b>
+      {value}
+    </p>
+  );
+};
+
+const MovieDetails = ({ selectedMovieDescription }) => {
+  return (
+    <div className={styles.details}>
+      <Block label={"Genre"} value={selectedMovieDescription.Genre} />
+      <Block label={"Country"} value={selectedMovieDescription.Country} />
+      <Block label={"Director"} value={selectedMovieDescription.Director} />
+      <Block label={"Released"} value={selectedMovieDescription.Released} />
+      <Block label={"Actors"} value={selectedMovieDescription.Actors} />
+      <Block label={"Plot"} value={selectedMovieDescription.Plot} />
+    </div>
+  );
+};
+
+export default MovieDetails;
